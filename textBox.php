474d26c4c44c3d587c4559585edab8ee62d4a520
Robby from Frank's Serer <geomasta21@aol.com>
2011-12-08 00:35:10 -0600

    when the server wasnt broadcasting

--- textBox.php
+++ textBox.php
@@ -7,8 +7,11 @@ mysql_connect ("localhost:3307","root","asdf123") or die ('Error: ' . mysql_erro
 
 mysql_select_db("testcloud") or die ('Data error:' . mysql_error());
 
-$textFormQuery = "INSERT INTO clouddata (id, text) VALUES ('$id', '$text')";
+$textFormQuery = 
+"REPLACE INTO clouddata (id, text) VALUES ('$id', '$text')";
 
 mysql_query($textFormQuery) or die ('Error updating database ' . mysql_error());
+
+echo "Submitted!";
 
 ?>
